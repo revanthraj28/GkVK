e0f2a52b4de8162f808a99a1acf00925978cc49f
Manju1925 <manjumsbng2003@gmail.com>
2024-06-11 20:08:58 +0530

    abcd

--- lib/views/Generate_id/Completedetails/GenerateFarmersid/GenerateFarmersId_view.dart
+++ lib/views/Generate_id/Completedetails/GenerateFarmersid/GenerateFarmersId_view.dart
@@ -4,11 +4,11 @@ import 'package:gkvk/shared/components/CustomTextButton.dart';
 import 'package:gkvk/shared/components/CustomTextFormField.dart';
 import 'package:gkvk/shared/components/SelectionButton.dart';
 import 'package:gkvk/database/farmer_profile_db.dart';
+import 'package:gkvk/views/Generate_id/detailsofCrops/Cropdetails/components/FertilizerForm.dart';
 import 'package:gkvk/views/Generate_id/detailsofCrops/Surveypages/Surveypages1.dart';
 
 class GenerateFarmersIdPage extends StatelessWidget {
   final int waterShedId; // Accept waterShedId as a required parameter
-
   final _farmerNameController = TextEditingController();
   final _fatherNameController = TextEditingController();
   final _pincodeController = TextEditingController();
@@ -49,7 +49,7 @@ class GenerateFarmersIdPage extends StatelessWidget {
       Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => const SurveyPage1(),
+          builder: (context) => SurveyPage1(aadharId: int.parse(_aadharController.text),),
         ),
       );
     } catch (e) {
